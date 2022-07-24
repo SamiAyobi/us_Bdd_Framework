d40a373906839a34ab31fec5df05588138d510af
unknown <sami.prt@gmail.com>
2022-07-24 14:56:23 -0700

    create new class by the name of practice

---
+++ src/main/java/page/objects/PracticePage.java
@@ -0,0 +1,16 @@
+package page.objects;
+
+public class PracticePage {
+
+	public static void main(String[] args) {
+		
+		
+		int a=10;
+		int b= 20;
+		int c =a+b;
+	 System.out.println(c);
+	
+
+	}
+
+}
